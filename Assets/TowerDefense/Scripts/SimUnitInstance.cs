9fec70b3db620c7d547165c7b227df2077e61a74
Ian Pilipski <ian@pilipski.com>
2015-05-11 19:20:31 -0400

    Added DoDamage and Damage Evaluation
    Added delayed damage evaluation to units and added to lockstep.

--- Assets/TowerDefense/Scripts/SimUnitInstance.cs
+++ Assets/TowerDefense/Scripts/SimUnitInstance.cs
@@ -4,7 +4,22 @@ using System.Collections.Generic;
 
 public class SimUnitInstance {
 
+	private struct DamageDef {
+		public float Amount;
+		public float Delay;
+
+		public DamageDef(float Amount, float Delay) {
+			this.Amount = Amount;
+			this.Delay = Delay;
+		}
+	}
+
 	private Simulation Sim;
+
+	public float Health {
+		get;
+		private set;
+	}
 
 	public SimUnit Unit {
 		get;
@@ -33,6 +48,7 @@ public class SimUnitInstance {
 
 	public SimUnitInstance(Simulation sim, SimUnit unit, Vector3 startpos, ISimUnitEventHandler handler) {
 		Sim = sim;
+		Health = sim.health;
 		oldposition = startpos;
 		position = startpos;
 		Unit = unit;
@@ -44,6 +60,22 @@ public class SimUnitInstance {
 	}
 
 	public void Step(float deltatime) {
+		EvaluateAttack(deltatime);
+		EvaluateMovement(deltatime);
+	}
+
+	private void EvaluateDamage(float deltatime) {
+		if(Health<=0.0f) {
+			ConditionalDropBonus();
+			OnExplode();
+		}
+	}
+
+	private void EvaluateAttack(float deltatime) {
+
+	}
+
+	private void EvaluateMovement(float deltatime) {
 		// Units lock step so only do movement updates here, and any deterministic logic
 		if(Unit.Movement!=null) {
 			Position = Unit.Movement.CalculatePosition(this, deltatime);
