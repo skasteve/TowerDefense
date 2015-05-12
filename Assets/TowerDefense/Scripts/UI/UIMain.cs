8a3856edeb293c25db357b37383d4f2d1f3938a4
skasteve <skasteve@gmail.com>
2015-05-12 11:14:30 -0400

    UI mockup

---
+++ Assets/TowerDefense/Scripts/UI/UIMain.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIMain : MonoBehaviour {
+
+	public GameObject startScreen;
+	public GameObject gameScreen;
+	public GameObject endScreen;
+
+	private GameObject _currentScreen;
+
+	void Awake()
+	{
+		_currentScreen = startScreen;
+		loadStartScreen();
+	}
+
+	public void unloadCurrentScreen()
+	{
+		_currentScreen.SetActive(false);
+	}
+
+	public void loadStartScreen()
+	{
+		unloadCurrentScreen();
+
+		startScreen.SetActive(true);
+		_currentScreen = startScreen;
+	}
+
+	public void loadGameScreen()
+	{
+		unloadCurrentScreen();
+
+		gameScreen.SetActive(true);
+		_currentScreen = gameScreen;
+	}
+
+	public void loadEndScreen()
+	{
+		unloadCurrentScreen();
+
+		endScreen.SetActive(true);
+		_currentScreen = endScreen;
+	}
+}
