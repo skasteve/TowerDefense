2fd0b541a2778ad19a4b45b9c59d8d186e4a7208
Riccardo Camarra <rcamarra@gsngames.com>
2015-05-12 18:44:30 -0400

    - replaced the unit placement plane with an actual collider so we can easily define bounds within the editor

--- Assets/TowerDefense/Scripts/UI/UnitPlacement.cs
+++ Assets/TowerDefense/Scripts/UI/UnitPlacement.cs
@@ -6,15 +6,7 @@ using System.Collections;
 /// </summary>
 public class UnitPlacement : MonoBehaviour
 {
-	public Transform placementPos;
-
-	private Plane _placementPlane;
 	private SimUnit _selectedUnitType = null;
-
-	void Awake()
-	{
-		_placementPlane = new Plane(Vector3.up, placementPos.position);
-	}
 
 	void Update()
 	{
@@ -22,9 +14,9 @@ public class UnitPlacement : MonoBehaviour
 		{ // cast a ray onto the placement plane to determine where to place a unit
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			float dist;
-			if (_placementPlane.Raycast(ray, out dist))
-				PlaceUnit(ray.GetPoint(dist));
+			RaycastHit raycastHit;
+			if (Physics.Raycast(ray, out raycastHit, float.PositiveInfinity, 1 << LayerMask.NameToLayer("PlacementPlane")))
+				PlaceUnit(ray.GetPoint(raycastHit.distance));
 		}
 	}
 
@@ -34,8 +26,10 @@ public class UnitPlacement : MonoBehaviour
 	/// <param name="pos">world position</param>
 	private void PlaceUnit(Vector3 pos)
 	{
-		if (_selectedUnitType != null)
-			Instantiate(_selectedUnitType.UnitPrefab, pos, Quaternion.identity);
+		if (_selectedUnitType == null)
+			return;
+
+		Instantiate(_selectedUnitType.UnitPrefab, pos, Quaternion.identity);
 	}
 
 	public void SelectUnit(SimUnit unit)
